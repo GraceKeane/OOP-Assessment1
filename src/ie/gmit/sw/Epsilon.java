429700ded7965b232380b82e80d81d836a57af0c
GraceKeane <gracekeane82@gmail.com>
2020-11-07 12:38:33 +0000

    Complex java classes linked

--- src/ie/gmit/sw/Epsilon.java
+++ src/ie/gmit/sw/Epsilon.java
@@ -1,7 +1,48 @@
 package ie.gmit.sw;
 
-public class Epsilon {
+public class Epsilon implements Theta{
 	public int epsilon(String value) {
 		return value.length();
+	}
+	
+	
+	public String epsilonTest() {
+		System.out.println("Epsilon.java is executing! " + "Class dddress: " + this.getClass().getName() + " Hash Code: " + hashCode());
+		return null;
+	}
+
+
+	@Override
+	public Object Theta() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public int delta(String value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public String deltaTest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public int zeta(String v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public String zetaTest() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
--- src/ie/gmit/sw/Zeta.java
+++ src/ie/gmit/sw/Zeta.java
@@ -1,7 +1,42 @@
 package ie.gmit.sw;
 
-public class Zeta {
+public class Zeta implements Theta {
 	public int zeta(String v) {
 		return Integer.parseInt(v) + 7;
+	}
+	
+	public String zetaTest() {
+		System.out.println("Zeta.java is executing! " + "Class dddress: " + this.getClass().getName() + " Hash Code: " + hashCode());
+		return null;
+	}
+
+	@Override
+	public Object Theta() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delta(String value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String deltaTest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String epsilonTest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int epsilon(String value) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
